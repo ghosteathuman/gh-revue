50f622977cee4a46ba5e4910734235aa78a483cf
ghosteathuman <46582963+ghosteathuman@users.noreply.github.com>
2021-02-25 20:46:13 +0800

    GitHub get_user_identity endpoint.

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,5 +1,18 @@
-class GitHubController < ApplicationControler
+class GithubController < ApplicationController
+  ENDPOINT = URI.parse("https://api.github.com")
+
   def get_user_identity
-    
+    client_id = Rails.application.credentials.github[:client_id]
+    # url = URI.parse("https://github.com/login/oauth/authorize")
+
+    puts Net::HTTP.get(
+      URI("https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo&redirect_uri=#{request.base_url}"),
+      {"Content-Type" => "application/json"}
+    )
+
+    # Net::HTTP.get(url, {
+    #   "client_id" => client_id, "scope" => "repo", "redirect_uri" => request.base_url
+    # }.to_json,
+    #   "Content-Type" => "application/json")
   end
 end
