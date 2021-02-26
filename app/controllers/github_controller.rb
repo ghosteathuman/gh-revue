236c4dd66c6d7c59a7c3f562956c57ff1b941a36
ghosteathuman <46582963+ghosteathuman@users.noreply.github.com>
2021-02-26 19:35:57 +0800

    Spiking on Github OAuthCallback.

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -3,16 +3,27 @@ class GithubController < ApplicationController
 
   def get_user_identity
     client_id = Rails.application.credentials.github[:client_id]
-    # url = URI.parse("https://github.com/login/oauth/authorize")
 
-    puts Net::HTTP.get(
-      URI("https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo&redirect_uri=#{request.base_url}"),
+    return_html = Net::HTTP.get(
+      URI("https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo&redirect_uri=#{request.base_url}/callback"),
       {"Content-Type" => "application/json"}
     )
 
-    # Net::HTTP.get(url, {
-    #   "client_id" => client_id, "scope" => "repo", "redirect_uri" => request.base_url
-    # }.to_json,
-    #   "Content-Type" => "application/json")
+    redirect_url = Nokogiri::HTML.parse(return_html).at_css("a")[:href]
+
+    redirect_to redirect_url
+  end
+
+  def callback
+    url = URI.parse("https://github.com/login/oauth/access_token")
+    client_id = Rails.application.credentials.github[:client_id]
+    client_secret = Rails.application.credentials.github[:client_secret]
+
+    response = Net::HTTP.post(url, {
+      "client_id" => client_id, "client_secret" => client_secret, "code" => params[:code]
+    }.to_json,
+      "Content-Type" => "application/json", "Accept" => "application/json")
+    response_body = JSON.parse(response.body)
+    access_token = response_body["access_token"]
   end
 end
